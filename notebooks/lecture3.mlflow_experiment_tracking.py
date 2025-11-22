923ae66fc0877e3b8b421aa8c7200916c9dd97f4
aparnaharichandran <aparnaharichandran@gmail.com>
2025-11-22 21:04:20 +0000

    install python-dotenv

--- notebooks/lecture3.mlflow_experiment_tracking.py
+++ notebooks/lecture3.mlflow_experiment_tracking.py
@@ -1,4 +1,8 @@
 # Databricks notebook source
+# MAGIC %pip install python-dotenv==1.1.1
+
+# COMMAND ----------
+
 import json
 import os
 
@@ -12,9 +16,11 @@ def is_databricks() -> bool:
     return "DATABRICKS_RUNTIME_VERSION" in os.environ
 
 # COMMAND ----------
+
 mlflow.get_tracking_uri()
 
 # COMMAND ----------
+
 if not is_databricks():
     load_dotenv()
     profile = os.environ.get("PROFILE")
@@ -22,21 +28,28 @@ if not is_databricks():
     mlflow.set_registry_uri(f"databricks-uc://{profile}")
 
 mlflow.get_tracking_uri()
+
 # COMMAND ----------
+
 experiment = mlflow.set_experiment(experiment_name="/Shared/marvel-demo")
 mlflow.set_experiment_tags({"repository_name": "marvelousmlops/marvel-characters"})
 
 print(experiment)
+
 # COMMAND ----------
+
 # dump class attributes in a json file for visualization
 os.makedirs("../demo_artifacts", exist_ok=True)
 with open("../demo_artifacts/mlflow_experiment.json", "w") as json_file:
     json.dump(experiment.__dict__, json_file, indent=4)
 
 # COMMAND ----------
+
 # get experiment by id
 mlflow.get_experiment(experiment.experiment_id)
+
 # COMMAND ----------
+
 # search for experiment
 experiments = mlflow.search_experiments(
     filter_string="tags.repository_name='marvelousmlops/marvel-characters'"
@@ -44,18 +57,22 @@ experiments = mlflow.search_experiments(
 print(experiments)
 
 # COMMAND ----------
+
 # start a run
 mlflow.start_run()
 
 # COMMAND ----------
+
 # get active run
 print(mlflow.active_run().__dict__)
 
 # COMMAND ----------
+
 mlflow.end_run()
 print(mlflow.active_run() is None)
 
 # COMMAND ----------
+
 # start a run
 with mlflow.start_run(
     run_name="marvel-demo-run",
@@ -65,21 +82,27 @@ with mlflow.start_run(
     run_id = run.info.run_id
     mlflow.log_params({"type": "marvel_demo"})
     mlflow.log_metrics({"metric1": 1.0, "metric2": 2.0})
+
 # COMMAND ----------
+
 print(mlflow.active_run() is None)
 
 # COMMAND ----------
+
 run_info = mlflow.get_run(run_id=run_id).to_dictionary()
 print(run_info)
 
 # COMMAND ----------
+
 with open("../demo_artifacts/run_info.json", "w") as json_file:
     json.dump(run_info, json_file, indent=4)
 
 # COMMAND ----------
+
 print(run_info["data"]["metrics"])
 
 # COMMAND ----------
+
 print(run_info["data"]["params"])
 
 # COMMAND ----------
@@ -92,16 +115,21 @@ run_info = mlflow.get_run(run_id=f"{run_id}").to_dictionary()
 print(run_info)
 
 # COMMAND ----------
+
 mlflow.start_run(run_id=run_id)
 
 # COMMAND ----------
+
 # this will fail: not allowed to overwrite value
 mlflow.log_param("type", "marvel_demo2")
+
 # COMMAND ----------
+
 mlflow.log_param(key="purpose", value="get_certified")
 mlflow.end_run()
 
 # COMMAND ----------
+
 # start another run and log other things
 mlflow.start_run(run_name="marvel-demo-run-extra",
                  tags={"git_sha": "1234567890abcd"},
@@ -116,6 +144,7 @@ mlflow.log_dict({"k": "v"}, "dict_example.json")
 mlflow.log_artifacts("../demo_artifacts", artifact_path="demo_artifacts")
 
 # COMMAND ----------
+
 # log figure
 import matplotlib.pyplot as plt
 
@@ -125,7 +154,9 @@ ax.plot([0, 1], [2, 3])
 mlflow.log_figure(fig, "figure.png")
 
 # log image dynamically
+
 # COMMAND ----------
+
 import numpy as np
 
 for i in range(0,3):
@@ -135,6 +166,7 @@ for i in range(0,3):
 mlflow.end_run()
 
 # COMMAND ----------
+
 # other ways
 from time import time
 
@@ -149,24 +181,31 @@ runs = mlflow.search_runs(
                   "metrics.metric3>0 AND "
                   "tags.mlflow.source.type!='JOB'"
 )
+
 # COMMAND ----------
+
 runs
 
 # COMMAND ----------
+
 # load objects
 artifact_uri = runs.artifact_uri[0]
 mlflow.artifacts.load_dict(f"{artifact_uri}/dict_example.json")
 # nested runs
 
 # COMMAND ----------
+
 mlflow.artifacts.load_image(f"{artifact_uri}/figure.png")
+
 # COMMAND ----------
+
 # download artifacts
 mlflow.artifacts.download_artifacts(
     artifact_uri=f"{artifact_uri}/demo_artifacts",
     dst_path="../downloaded_artifacts")
 
 # COMMAND ----------
+
 # nested runs: useful for hyperparameter tuning
 with mlflow.start_run(run_name="marvel_top_level_run") as run:
     for i in range(1,5):
@@ -176,3 +215,5 @@ with mlflow.start_run(run_name="marvel_top_level_run") as run:
                                 "m3": 3+1.5*i})
 
 # COMMAND ----------
+
+
